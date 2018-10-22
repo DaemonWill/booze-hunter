669b016ac0ca985b74bd0186cc3405e2100e990a
Daimen Williams <daimenwill@gmail.com>
2018-10-22 05:11:06 -0400

    added store model

---
+++ src/app/models/store.ts
@@ -0,0 +1,49 @@
+export class Store {
+  private name : string;
+  private address : string;
+  private id : number;
+  private latitude : number;
+  private longitude : number;
+
+  constructor(newName: string, newAdd: string, newId: number, newLat: number,
+              newLon: number){
+      this.name = newName;
+      this.address = newAdd;
+      this.id = newId;
+      this.latitude = newLat;
+      this.longitude = newLon;
+  };
+
+  //getters & setters
+  public getName(){
+    return this.name;
+  };
+  public getAddress(){
+    return this.producerName;
+  };
+  public getId(){
+    return this.id;
+  };
+  public getLatitude(){
+    return this.latitude;
+  };
+  public getLongitude(){
+    return this.longitude;
+  };
+
+  public setName(newVal: string){
+    this.name = newVal;
+  };
+  public setAddress(newVal: string){
+    this.address = newVal;
+  };
+  public setId(newVal: number){
+    this.id = newVal;
+  };
+  public setLatitude(newVal: number){
+    this.latitude = newVal;
+  };
+  public setLongitude(newVal: number){
+    this.longitude = newVal;
+  };
+}
